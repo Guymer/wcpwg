7e5f08c0274ea2744c18ba22ff8761690642243f
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2019-10-22 14:06:35 +0100

    prettier

--- printBoundingBoxes.py
+++ printBoundingBoxes.py
@@ -21,11 +21,11 @@ shape_file = cartopy.io.shapereader.natural_earth(
 for record in cartopy.io.shapereader.Reader(shape_file).records():
     # Check if this country is one that I want ...
     if record.attributes["NAME"] in ["Chile", "United States of America"]:
-        print(record.attributes["NAME"])
+        print("{:s}:".format(record.attributes["NAME"]))
         for geometry in record.geometry:
             x1, y1, x2, y2 = geometry.bounds
             print(
-                "{:+6.1f} <= x <= {:+6.1f}    {:+6.1f} <= y <= {:+6.1f}".format(
+                "    {:+6.1f} <= x <= {:+6.1f}    {:+6.1f} <= y <= {:+6.1f}".format(
                     math.floor(x1),
                     math.ceil(x2),
                     math.floor(y1),

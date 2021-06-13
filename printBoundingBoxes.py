c4c081b32c467cd3dc65ed474a3a4e6ee035caa7
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2021-06-13 18:09:42 +0100

    bugfix: Colorado is a Polygon, not a MultiPolygon

--- printBoundingBoxes.py
+++ printBoundingBoxes.py
@@ -47,13 +47,12 @@ for record in cartopy.io.shapereader.Reader(shape_file).records():
     # Check if this state/province is one that I want ...
     if record.attributes["name"] in ["Colorado"]:
         print("{:s}:".format(record.attributes["name"]))
-        for geometry in record.geometry:
-            x1, y1, x2, y2 = geometry.bounds
-            print(
-                "    {:+6.1f} <= x <= {:+6.1f}    {:+6.1f} <= y <= {:+6.1f}".format(
-                    math.floor(x1),
-                    math.ceil(x2),
-                    math.floor(y1),
-                    math.ceil(y2)
-                )
+        x1, y1, x2, y2 = record.geometry.bounds
+        print(
+            "    {:+6.1f} <= x <= {:+6.1f}    {:+6.1f} <= y <= {:+6.1f}".format(
+                math.floor(x1),
+                math.ceil(x2),
+                math.floor(y1),
+                math.ceil(y2)
             )
+        )

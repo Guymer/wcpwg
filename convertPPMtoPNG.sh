df74c04888995822dd4831b3da82ad232023286e
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2021-12-19 15:44:38 +0000

    added header

--- convertPPMtoPNG.sh
+++ convertPPMtoPNG.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
+# Check that non-standard programs are installed. "standard" programs are
+# anything that is specified in the POSIX.1-2008 standard (and the IEEE Std
+# 1003.1 standard) or that is a BASH builtin command. Therefore, "non-standard"
+# programs are anything that does not appear on the following two lists:
+#   * https://pubs.opengroup.org/onlinepubs/9699919799/idx/utilities.html
+#   * https://www.gnu.org/software/bash/manual/html_node/Bash-Builtins.html
+if ! type convert &> /dev/null; then
+    echo "ERROR: \"convert\" is not installed." >&2
+    exit 1
+fi
+if ! type exiftool &> /dev/null; then
+    echo "ERROR: \"exiftool\" is not installed." >&2
+    exit 1
+fi
+if ! type optipng &> /dev/null; then
+    echo "ERROR: \"optipng\" is not installed." >&2
+    exit 1
+fi
+
 # Loop over PPM images ...
 for ppm in *.ppm; do
     # Skip those that do not exist ...
@@ -12,6 +31,7 @@ for ppm in *.ppm; do
     if [[ $ppm -nt $png ]]; then
         echo "Making \"$png\" ..."
 
+        # Make PNG ...
         convert "$ppm" "$png"
         optipng "$png"
         exiftool -overwrite_original -all= "$png"

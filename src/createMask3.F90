79ecb4d3533d8086a14ce20ba6dc03f6b2802d13
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2025-07-12 12:22:16 +0100

    standardise scale and use it in the file name

--- src/createMask3.F90
+++ src/createMask3.F90
@@ -1,6 +1,8 @@
 PROGRAM main
-    ! Import modules ...
+    ! Import standard modules ...
     USE ISO_FORTRAN_ENV
+
+    ! Import my modules ...
     USE mod_funcs
     USE mod_safe,           ONLY:   sub_allocate_array,                         &
                                     sub_load_array_from_BIN,                    &
@@ -10,19 +12,19 @@ PROGRAM main
     IMPLICIT NONE
 
     ! Declare parameters ...
-    INTEGER(kind = INT64), PARAMETER                                            :: nmax = 200_INT64
-    INTEGER(kind = INT64), PARAMETER                                            :: nmax_thread = 1000_INT64
+    INTEGER(kind = INT64), PARAMETER                                            :: nIterMax = 200_INT64
+    INTEGER(kind = INT64), PARAMETER                                            :: nIterMax_thread = 1000_INT64
     INTEGER(kind = INT64), PARAMETER                                            :: nx = 43200_INT64
     INTEGER(kind = INT64), PARAMETER                                            :: ny = 21600_INT64
-    INTEGER(kind = INT64), PARAMETER                                            :: scale = 20_INT64
+    INTEGER(kind = INT64), PARAMETER                                            :: tileScale = 32_INT64
 
     ! Declare variables ...
-    CHARACTER(len = 33)                                                         :: bname
-    CHARACTER(len = 33)                                                         :: iname
+    CHARACTER(len = 256)                                                        :: bname
+    CHARACTER(len = 256)                                                        :: iname
     LOGICAL(kind = INT8), ALLOCATABLE, DIMENSION(:, :)                          :: mask
     INTEGER(kind = INT16), ALLOCATABLE, DIMENSION(:, :)                         :: elev
-    INTEGER(kind = INT64)                                                       :: i
-    INTEGER(kind = INT64)                                                       :: i_thread
+    INTEGER(kind = INT64)                                                       :: iIter
+    INTEGER(kind = INT64)                                                       :: iIter_thread
     INTEGER(kind = INT64)                                                       :: ix
     INTEGER(kind = INT64)                                                       :: ixlo
     INTEGER(kind = INT64)                                                       :: ixhi
@@ -40,19 +42,23 @@ PROGRAM main
     INTEGER(kind = INT32)                                                       :: funit
 
     ! Check scale ...
-    IF(MOD(nx, scale) /= 0_INT64)THEN
-        WRITE(fmt = '("ERROR: ", a, ".")', unit = ERROR_UNIT) '"nx" is not an integer multiple of "scale"'
+    IF(MOD(nx, tileScale) /= 0_INT64)THEN
+        WRITE(fmt = '("ERROR: ", a, ".")', unit = ERROR_UNIT) '"nx" is not an integer multiple of "tileScale"'
         FLUSH(unit = ERROR_UNIT)
         STOP
     END IF
-    IF(MOD(ny, scale) /= 0_INT64)THEN
-        WRITE(fmt = '("ERROR: ", a, ".")', unit = ERROR_UNIT) '"ny" is not an integer multiple of "scale"'
+    IF(MOD(ny, tileScale) /= 0_INT64)THEN
+        WRITE(fmt = '("ERROR: ", a, ".")', unit = ERROR_UNIT) '"ny" is not an integer multiple of "tileScale"'
         FLUSH(unit = ERROR_UNIT)
         STOP
     END IF
 
     ! Ensure that the output directory exists ...
-    CALL EXECUTE_COMMAND_LINE("mkdir -p ../createMask3output", CMDMSG = errmsg, EXITSTAT = errnum)
+    CALL EXECUTE_COMMAND_LINE(                                                  &
+        "mkdir -p ../createMask3output",                                        &
+          cmdmsg = errmsg,                                                      &
+        exitstat = errnum                                                       &
+    )
     IF(errnum /= 0_INT32)THEN
         WRITE(fmt = '("ERROR: ", a, ". ERRMSG = ", a, ". ERRNUM = ", i3, ".")', unit = ERROR_UNIT) "Failed to make output directory", TRIM(errmsg), errnum
         FLUSH(unit = ERROR_UNIT)
@@ -63,10 +69,9 @@ PROGRAM main
     CALL sub_allocate_array(elev, "elev", nx, ny, .TRUE._INT8)
     CALL sub_load_array_from_BIN(elev, "../all10g.bin")                         ! [m]
 
-    ! Allocate (889.89 MiB) array ...
+    ! Allocate (889.89 MiB) array and initialize it to allow pregnant women to
+    ! go anywhere <= 2,500m ASL ...
     CALL sub_allocate_array(mask, "mask", nx, ny, .TRUE._INT8)
-
-    ! Initialize mask to allow pregnant women to go anywhere <= 2,500m ASL ...
     mask = elev <= 2500_INT16
 
     ! Print progress ...
@@ -77,13 +82,21 @@ PROGRAM main
     CALL sub_save_array_as_BIN(mask, "../createMask3output/before.bin")
     CALL sub_save_array_as_PBM(nx, ny, mask, "../createMask3output/before.pbm")
 
-    ! Initialize mask to not allow pregnant women to go anywhere except the
+    ! Re-initialize mask to not allow pregnant women to go anywhere except the
     ! top-left corner ...
     mask = .FALSE._INT8
     mask(1, 1) = .TRUE._INT8
 
     ! Open CSV ...
-    OPEN(action = "write", file = "../createMask3.csv", form = "formatted", iomsg = errmsg, iostat = errnum, newunit = funit, status = "replace")
+    OPEN(                                                                       &
+         action = "write",                                                      &
+           file = "../createMask3.csv",                                         &
+           form = "formatted",                                                  &
+          iomsg = errmsg,                                                       &
+         iostat = errnum,                                                       &
+        newunit = funit,                                                        &
+         status = "replace"                                                     &
+    )
     IF(errnum /= 0_INT32)THEN
         WRITE(fmt = '("ERROR: ", a, ". ERRMSG = ", a, ". ERRNUM = ", i3, ".")', unit = ERROR_UNIT) "Failed to open BIN", TRIM(errmsg), errnum
         FLUSH(unit = ERROR_UNIT)
@@ -95,14 +108,14 @@ PROGRAM main
     FLUSH(unit = funit)
 
     ! Start ~infinite loop ...
-    DO i = 1_INT64, nmax
+    DO iIter = 1_INT64, nIterMax
         ! Print progress ...
-        WRITE(fmt = '("Calculating step ", i4, " of (up to) ", i4, " ...")', unit = OUTPUT_UNIT) i, nmax
+        WRITE(fmt = '("Calculating step ", i4, " of (up to) ", i4, " ...")', unit = OUTPUT_UNIT) iIter, nIterMax
         FLUSH(unit = OUTPUT_UNIT)
 
         ! Create file names ...
-        WRITE(bname, '("../createMask3output/mask", i4.4, ".bin")') i
-        WRITE(iname, '("../createMask3output/mask", i4.4, ".ppm")') i
+        WRITE(bname, '("../createMask3output/mask", i4.4, "_scale=", i2.2, "km.bin")') iIter, tileScale
+        WRITE(iname, '("../createMask3output/mask", i4.4, "_scale=", i2.2, "km.ppm")') iIter, tileScale
 
         ! Find initial total ...
         oldtot = COUNT(mask, kind = INT64)
@@ -112,7 +125,7 @@ PROGRAM main
 
         !$omp parallel                                                          &
         !$omp default(none)                                                     &
-        !$omp private(i_thread)                                                 &
+        !$omp private(iIter_thread)                                             &
         !$omp private(ix)                                                       &
         !$omp private(ixlo)                                                     &
         !$omp private(ixhi)                                                     &
@@ -126,24 +139,24 @@ PROGRAM main
             !$omp do                                                            &
             !$omp schedule(dynamic)
                 ! Loop over x-axis tiles ...
-                DO ix = 1_INT64, nx / scale
+                DO ix = 1_INT64, nx / tileScale
                     ! Find the extent of the tile ...
-                    ixlo = (ix - 1_INT64) * scale + 1_INT64
-                    ixhi =  ix            * scale
+                    ixlo = (ix - 1_INT64) * tileScale + 1_INT64
+                    ixhi =  ix            * tileScale
 
                     ! Loop over y-axis tiles ...
-                    DO iy = 1_INT64, ny / scale
+                    DO iy = 1_INT64, ny / tileScale
                         ! Find the extent of the tile ...
-                        iylo = (iy - 1_INT64) * scale + 1_INT64
-                        iyhi =  iy            * scale
+                        iylo = (iy - 1_INT64) * tileScale + 1_INT64
+                        iyhi =  iy            * tileScale
 
                         ! Start ~infinite loop ...
-                        DO i_thread = 1_INT64, nmax_thread
+                        DO iIter_thread = 1_INT64, nIterMax_thread
                             ! Find initial total ...
                             oldtot_thread = COUNT(mask(ixlo:ixhi, iylo:iyhi), kind = INT64)
 
                             ! Stop looping once no changes can be made ...
-                            IF(oldtot_thread == 0_INT64 .OR. oldtot_thread == scale ** 2)THEN
+                            IF(oldtot_thread == 0_INT64 .OR. oldtot_thread == tileScale ** 2)THEN
                                 EXIT
                             END IF
 
@@ -167,11 +180,18 @@ PROGRAM main
         newtot = COUNT(mask, kind = INT64)
 
         ! Write progress ...
-        WRITE(fmt = '(i3, ",", i9)', unit = funit) i, newtot
+        WRITE(fmt = '(i3, ",", i9)', unit = funit) iIter, newtot
         FLUSH(unit = funit)
 
         ! Save shrunk mask ...
-        CALL saveShrunkMask(nx, ny, mask, scale, bname, iname)
+        CALL saveShrunkMask(                                                    &
+                   nx = nx,                                                     &
+                   ny = ny,                                                     &
+                 mask = mask,                                                   &
+            tileScale = tileScale,                                              &
+                bname = TRIM(bname),                                            &
+                iname = TRIM(iname)                                             &
+        )
 
         ! Stop looping once no changes have been made ...
         IF(newtot == oldtot)THEN

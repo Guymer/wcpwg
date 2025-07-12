0792c2073ba0dd6c5bcbd4b618eb04d1a6b574b4
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2025-07-12 14:21:45 +0100

    add command-line arguments for the download

--- step1_downloadGLOBE.py
+++ step1_downloadGLOBE.py
@@ -4,6 +4,7 @@
 # NOTE: See https://docs.python.org/3.12/library/multiprocessing.html#the-spawn-and-forkserver-start-methods
 if __name__ == "__main__":
     # Import standard modules ...
+    import argparse
     import os
 
     # Import my modules ...
@@ -14,6 +15,27 @@ if __name__ == "__main__":
 
     # **************************************************************************
 
+    # Create argument parser and parse the arguments ...
+    parser = argparse.ArgumentParser(
+           allow_abbrev = False,
+            description = "Download the GLOBE dataset.",
+        formatter_class = argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--debug",
+        action = "store_true",
+          help = "print debug messages",
+    )
+    parser.add_argument(
+        "--timeout",
+        default = 60.0,
+           help = "the timeout for any requests/subprocess calls (in seconds)",
+           type = float,
+    )
+    args = parser.parse_args()
+
+    # **************************************************************************
+
     # Check if the ZIP file does not exist yet ...
     if not os.path.exists("all10g.zip"):
         print("Downloading \"all10g.zip\" ...")
@@ -21,5 +43,12 @@ if __name__ == "__main__":
         # Start session ...
         with pyguymer3.start_session() as sess:
             # Download the ZIP file ...
-            if not pyguymer3.download_file(sess, "https://www.ngdc.noaa.gov/mgg/topo/DATATILES/elev/all10g.zip", "all10g.zip"):
+            if not pyguymer3.download_file(
+                sess,
+                "https://www.ngdc.noaa.gov/mgg/topo/DATATILES/elev/all10g.zip",
+                "all10g.zip",
+                  debug = args.debug,
+                timeout = args.timeout,
+                 verify = True,
+            ):
                 raise Exception("download failed", "https://www.ngdc.noaa.gov/mgg/topo/DATATILES/elev/all10g.zip") from None

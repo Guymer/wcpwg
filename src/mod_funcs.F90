f17cec5d8fe9745d7f6a39448d04fcf2d0c7dcdb
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2026-04-26 13:06:57 +0100

    vectorise output

--- src/mod_funcs.F90
+++ src/mod_funcs.F90
@@ -1,6 +1,16 @@
 MODULE mod_funcs
     CONTAINS
 
+    ! Include subroutines ...
+    INCLUDE "mod_funcs/sub_go_east.f90"
+    INCLUDE "mod_funcs/sub_go_north.f90"
+    INCLUDE "mod_funcs/sub_go_south.f90"
+    INCLUDE "mod_funcs/sub_go_west.f90"
+    INCLUDE "mod_funcs/sub_going_east.f90"
+    INCLUDE "mod_funcs/sub_going_north.f90"
+    INCLUDE "mod_funcs/sub_going_south.f90"
+    INCLUDE "mod_funcs/sub_going_west.f90"
+
     PURE SUBROUTINE incrementMask(nx, ny, elev, mask, ixlo, ixhi, iylo, iyhi)
         ! Import standard modules ...
         USE ISO_FORTRAN_ENV
